240d9017ea2b29d180021d2bcf906d8f94cfa086
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-03 17:09:25 +0300

    add comments

--- YandexPracticum/11th_sprint/Elegance and Scalability/Files Working With/5 Binary files: Reading and Writing Bytes/CompressorRLE/decompressor.h
+++ YandexPracticum/11th_sprint/Elegance and Scalability/Files Working With/5 Binary files: Reading and Writing Bytes/CompressorRLE/decompressor.h
@@ -6,12 +6,19 @@
 
 inline bool DecodeRLE(const std::string& src_name, const std::string& dst_name) {
 	using namespace std;
+	//Action if file does not exist: Create new
+	//Action if file already exists: Write to end
 	ifstream in_file_rle(src_name, ios::in | ios::binary);
+	//если файл не прочитан
 	if (!in_file_rle) {
 		return false;
 	}
+	//записываем в выходной поток
+	//Action if file does not exist: Create new
+	//Action if file already exists: Write to end
 	ofstream out_file_decode(dst_name, ios::out | ios::binary);
 	do {
+		//считали кол-во байт во входном потоке
 		int byte = in_file_rle.get();
 		bool bit_0 = byte & (1 << 0);
 		if (bit_0 == 0) {
