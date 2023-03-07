10c7c34b6201da373a2a1f82933b536815d8773c
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-07 18:54:55 +0300

    Create Sorted_IILE.cpp

---
+++ YandexPracticum/11th_sprint/Elegance and Scalability/7 Constancy as Element of Program Design/4 Immediately Invoked Lambda Expression Idiom/Sorted_IILE.cpp
@@ -0,0 +1,32 @@
+#include<vector>
+#include<algorithm>
+#include<iostream>
+
+using namespace std;
+
+vector<int> Sorted(vector<int> nums) {
+    sort(nums.begin(), nums.end());
+    return nums;
+}
+
+int main() {
+    
+    //Применение немедленно вызываемой лямбды
+    //immediately invoked lambda expression
+    const vector<int> sorted_v = [] {
+        vector<int> nums = Sorted({ 1, 5, 9, 7, 15, 6, 1, 5, 9 });
+        auto it = unique(nums.begin(), nums.end());
+        nums.erase(it, nums.end());
+        return nums;
+    }();
+
+    int n;
+    // прочитаем n чисел из потока ввода и найдём их в sorted_v
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        cout << x << ' ';
+        cout << std::binary_search(sorted_v.begin(), sorted_v.end(), x) << endl;
+    }
+}
