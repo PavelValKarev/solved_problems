af07e2974f210d1f8feac9641602dc1d17023db8
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-08 14:31:32 +0300

    Update flags_write_position.cpp

---
+++ YandexPracticum/11th_sprint/Elegance and Scalability/8 Files Working With/4 Files Opening Details/flags_write_position.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+using namespace std;
+
+int main(int argc, const char** argv) {
+
+    setlocale(LC_ALL, "Ru");
+
+    //  1
+    {
+        fstream fout("telefon.txt", ios::out);
+        fout << "У меня зазвонил телефон."sv << endl;
+        fout << "- Кто говорит?"sv << endl;
+    }
+
+    // 2
+    {
+        //запись в начало файла (перезаписывает текст поверх старого)
+        fstream fout("telefon.txt", ios::in | ios::out);
+        //добавляем позицию записи
+        fout.seekp(72);
+        fout << "- Слон."s << endl;
+        //tellp() сообщает позицию: 
+        cout << "Writing at pos: "s << fout.tellp() << endl;
+    }
+
+    // 3
+    {
+        fstream fin("telefon.txt", ios::in);
+        string str;
+        while (getline(fin, str)) {
+            cout << str << endl;
+        }
+    }
+}
