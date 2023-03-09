48bec0900399d96c89ba7fd6a3a1f7b18bfd017f
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-09 17:53:06 +0300

    Create json.h

---
+++ YandexPracticum/12th_sprint/Elegance and Scalability/8 Files Working With/9 RAII idiom/6 Building JSON/Task3_3/json.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace json {
+
+class Node;
+using Dict = std::map<std::string, Node>;
+using Array = std::vector<Node>;
+
+class ParsingError : public std::runtime_error {
+public:
+    using runtime_error::runtime_error;
+};
+
+class Node final
+    : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
+public:
+    using variant::variant;
+    using Value = variant;
+
+    bool IsInt() const;
+    bool IsPureDouble() const;
+    bool IsDouble() const;
+    bool IsBool() const;
+    bool IsNull() const;
+    bool IsArray() const;
+    bool IsString() const;
+    bool IsDict() const;
+
+    int AsInt() const;
+    double AsDouble() const;
+    bool AsBool() const;
+    const Array& AsArray() const;
+    const std::string& AsString() const;
+    const Dict& AsDict() const;
+        
+    Array& AsArray();
+    std::string& AsString();
+    Dict& AsDict();
+
+    bool operator==(const Node& rhs) const;
+
+    const Value& GetValue() const {
+        return *this;
+    }
+};
+
+inline bool operator!=(const Node& lhs, const Node& rhs);
+
+class Document {
+public:
+    explicit Document(Node root);
+    const Node& GetRoot() const;
+    Node& GetRoot();
+
+private:
+    Node root_;
+};
+
+inline bool operator==(const Document& lhs, const Document& rhs);
+inline bool operator!=(const Document& lhs, const Document& rhs);
+Document Load(std::istream& input);
+void Print(const Document& doc, std::ostream& output);
+
+}  // namespace json
