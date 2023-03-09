76e0b4f918c3e2fc602bc7ee6070e7abeaf18ea4
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-09 17:06:32 +0300

    create unique_ptr cats by function

---
+++ YandexPracticum/11th_sprint/CreateCat.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+struct Cat {
+    Cat(const string& name, int age)
+        : name_(name)
+        , age_(age)  //
+    {
+        cout << name_ << " cat was created"s << endl;
+    }
+    const string& GetName() const noexcept {
+        return name_;
+    }
+    int GetAge() const noexcept {
+        return age_;
+    }
+    ~Cat() {
+        cout << name_ << " cat was destroyed"s << endl;
+    }
+    void Speak() const {
+        cout << "Meow!"s << endl;
+    }
+
+private:
+    string name_;
+    int age_;
+};
+
+// Функция создаёт двухлетних котов с заданным именем
+unique_ptr<Cat> CreateCat(const string& name) {
+    return make_unique<Cat>(name, 2);
+}
+
+int main() {
+    
+    auto cat1 = CreateCat("Tom"s);
+    
+    unique_ptr<Cat> cat2 = std::move(cat1);
+
+    assert(!cat1);
+}
