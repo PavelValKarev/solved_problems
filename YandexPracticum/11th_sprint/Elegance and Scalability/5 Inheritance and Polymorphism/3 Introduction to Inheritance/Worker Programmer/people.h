f62ffa34a56dba6e7f95e6fe84c90560c69dd060
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-09 23:11:58 +0300

    Create people.h

---
+++ YandexPracticum/11th_sprint/Elegance and Scalability/5 Inheritance and Polymorphism/3 Introduction to Inheritance/Worker Programmer/people.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <algorithm>
+
+enum class ProgrammingLanguage { CPP, JAVA, PYTHON, PHP };
+
+enum class Gender { MALE, FEMALE };
+
+class Person{
+    std::string name_;
+    int age_;
+    Gender gender_;
+    
+    public:
+    Person(const std::string& name, int age, Gender gender):
+    name_(name), age_(age), gender_(gender)
+    {
+        
+    }
+    
+    const std::string& GetName() const;
+    int GetAge() const;
+    Gender GetGender() const;
+};
+
+// Программист. Знает несколько языков программирования
+class Programmer: public Person {
+    std::vector<ProgrammingLanguage> langs;
+public:
+    Programmer(const std::string& name, int age, Gender gender):
+    Person(name, age, gender){};
+
+    // Добавляет программисту знание языка программирования language
+    // После вызова этого метода программист может программировать на этом языке
+    // и на ранее добавленных
+    void AddProgrammingLanguage(ProgrammingLanguage language);
+
+    // Сообщает, может ли программист программировать на языке прогарммирования language
+    // Изначально программист не владеет ни одним языком программирования
+    bool CanProgram(ProgrammingLanguage language) const;
+};
+
+enum class WorkerSpeciality { BLACKSMITH, CARPENTER, WOOD_CHOPPER, ENGINEER, PLUMBER };
+
+// Рабочий. Владеет несколькими специальностями
+class Worker: public Person {
+    std::vector<WorkerSpeciality> specs;
+public:
+    Worker(const std::string& name, int age, Gender gender):
+    Person(name, age, gender){};
+
+    // Добавляет рабочему способность работать по специальности speciality
+    void AddSpeciality(WorkerSpeciality speciality);
+
+    // Сообщает, может ли рабочий работать по указанной специальности.
+    // Сразу после своего создания рабочий не владеет никакими специальностями
+    bool HasSpeciality(WorkerSpeciality speciality) const;
+};
