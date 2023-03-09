3a2af8a7f3a936fbda9d96c7713460a1cc3abcfa
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-09 16:43:16 +0300

    simple way to make path with '\' and folders

---
+++ YandexPracticum/12th_sprint/Elegance and Scalability/8 Files Working With/6 Thorny paths: working with the file system/filesystem_path.cpp
@@ -0,0 +1,13 @@
+#include <filesystem>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    using filesystem::path;
+
+    path p = path("a") / path("folder") / path("and") / path("..") / path("a") / path("file.txt");
+
+    // выводим естественное представление пути в std::string
+    cout << p.string() << endl;
+}
