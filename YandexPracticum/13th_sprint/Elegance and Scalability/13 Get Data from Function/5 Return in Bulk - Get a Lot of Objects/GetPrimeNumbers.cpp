d30e5e2b32d052f5fc966b353a491204c23c9c4f
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-25 21:20:05 +0300

    get prime numbers function

---
+++ YandexPracticum/13th_sprint/Elegance and Scalability/13 Get Data from Function/5 Return in Bulk - Get a Lot of Objects/GetPrimeNumbers.cpp
@@ -0,0 +1,42 @@
+/*
+Ещё один пример необычного итератора, который можно использовать
+для возврата из функции — ostream_iterator из заголовка <iterator>.
+Полученные данные он пишет напрямую в указанный поток.
+
+Напишем функцию, выдающую простые числа на заданном диапазоне:
+*/
+
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+// не самый эффективный тест на простоту числа
+bool IsPrime(int n) {
+    if (std::abs(n) < 2) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template<typename OutputIt>
+void GetPrimeNumbers(int from, int to, OutputIt output_iter) {
+    for (int i = from; i < to; ++i) {
+        if (IsPrime(i)) {
+            *output_iter++ = i;
+        }
+    }
+}
+
+int main() {
+    // результат выведен в cout через пробел
+    GetPrimeNumbers(0, 100, ostream_iterator<int>(cout, " "));
+}
