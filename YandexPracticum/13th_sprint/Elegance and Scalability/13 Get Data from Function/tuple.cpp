fb45a9923e9c64b22c67d9476dea5de33326932a
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-25 10:29:46 +0300

    Delete tuple.cpp

--- YandexPracticum/13th_sprint/Elegance and Scalability/13 Get Data from Function/tuple.cpp
+++
@@ -1,6 +0,0 @@
-class Cohort {
-public:
-    using Raiting = int;
-    using Clock = std::chrono::system_clock;
-    std::tuple<const Student&, Raiting, Clock::time_point> FindBestStudent() const;
-};
