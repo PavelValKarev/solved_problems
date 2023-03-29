ecd87cf50c8777740f610e8f5e64a092654e7d78
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-03-29 22:29:11 +0300

    Delete comments.cpp

--- YandexPracticum/13th_sprint/Elegance and Scalability/14 Refactoring/3 Constructors for Advanced/comments.cpp
+++
@@ -1,8 +0,0 @@
-//вызов конструктора с непонятными параметрами
-ReadingManager manager(20000, 1000, 2);
-
-//чтобы внести ясность можно добавить комментарии
-ReadingManager manager(
-	/* max_user_count */ 20000,
-	/* max_page_count */ 1000,
-	/* cheer_factor */   2);
