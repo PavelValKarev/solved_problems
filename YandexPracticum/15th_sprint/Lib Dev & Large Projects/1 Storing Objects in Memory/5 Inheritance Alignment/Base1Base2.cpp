26abb6474aa95259480e75a285b3b22e57fd1d8a
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-11 00:25:53 +0300

    Create Base1Base2.cpp

---
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/1 Storing Objects in Memory/5 Inheritance Alignment/Base1Base2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+struct Base1 {
+    int i;
+    char c;
+};
+
+struct Base2 {
+    bool b;
+    float f;
+};
+
+struct Outer : Base1, Base2 {
+    short sh;
+};
+
+//Макрос offsetof невозможно применить к полям базового класса. 
+int main() {
+    cout << sizeof(Outer) << " "s
+        << sizeof(Base1) << " "s
+        << sizeof(Base2) << endl;
+}
