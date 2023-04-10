309d9c938a46470eefb3461527fc1043fe820866
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-10 22:17:34 +0300

    byte offset relative to the beginning of object

---
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/1 Storing Objects in Memory/2 Change the terms, the sum/offsetof.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+struct Plate {
+    char c1;
+    int num;
+    char c2;
+    char c3;
+    int region;
+};
+#include <iostream>
+
+int main() {
+    cout << "Sizeof = "s << sizeof(Plate) << endl;
+    cout << offsetof(Plate, c1) << endl;
+    cout << offsetof(Plate, num) << endl;
+    cout << offsetof(Plate, c2) << endl;
+    cout << offsetof(Plate, c3) << endl;
+    cout << offsetof(Plate, region) << endl;
+}
+/*
+Используем макрос offsetof. Он позволяет узнать, какое 
+смещение в байтах имеет поле относительно начала объекта:
+*/
