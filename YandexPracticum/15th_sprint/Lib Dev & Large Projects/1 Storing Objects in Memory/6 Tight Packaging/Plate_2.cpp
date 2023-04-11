514cacc2a7c230e003b68a9e7eeb224244415453
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-11 11:37:42 +0300

    12 bytes after change fields places

---
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/1 Storing Objects in Memory/6 Tight Packaging/Plate_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+struct Plate {    
+    int num;
+    int region;
+    char c1;
+    char c2;
+    char c3;    
+};
+
+//Макрос offsetof невозможно применить к полям базового класса. 
+int main() {
+    cout << "Sizeof = "s << sizeof(Plate) << endl;
+    
+    cout << offsetof(Plate, num) << endl;
+    cout << offsetof(Plate, region) << endl;
+    cout << offsetof(Plate, c1) << endl;
+    cout << offsetof(Plate, c2) << endl;
+    cout << offsetof(Plate, c3) << endl;
+    
+}
