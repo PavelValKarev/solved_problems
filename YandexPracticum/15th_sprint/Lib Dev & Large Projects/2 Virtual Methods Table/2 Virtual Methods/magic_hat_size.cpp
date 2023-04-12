d580cdcdfda6a4b0f273f5b5fdf64228641f1b70
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-12 10:08:46 +0300

    MagicHat size is 8 bytes

---
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/2 Virtual Methods Table/2 Virtual Methods/magic_hat_size.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class MagicHat {
+public:
+    virtual void WhatInside() const {
+        cout << "Nothing"s << endl;
+    }
+};
+
+class HatWithApples : public MagicHat {
+public:
+    void WhatInside() const {
+        cout << "Apple"s << endl;
+    }
+};
+
+class HatWithRabbits : public MagicHat {
+public:
+    void WhatInside() const {
+        cout << "Rabbit"s << endl;
+    }
+};
+
+class HatWithDoves : public MagicHat {
+public:
+    void WhatInside() const {
+        cout << "Dove"s << endl;
+    }
+};
+
+void FocusPocus(const vector<MagicHat*> hats) {
+    cout << "Let the magic begin..."s << endl;
+    for (auto hat : hats) {
+        hat->WhatInside();
+    }
+    //измерим шляпу
+    cout << "But the size of magic hat is "s << sizeof(MagicHat) << endl;
+}
+
+int main() {
+    HatWithApples hat1;
+    HatWithRabbits hat2;
+    HatWithDoves hat3;
+    vector<MagicHat*> hats = { &hat1, &hat2, &hat3 };
+    FocusPocus(hats);
+}
