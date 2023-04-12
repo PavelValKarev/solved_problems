51e75e77bc5e45e59a7b4260724725488e4590a9
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-13 01:44:45 +0300

    use using

--- YandexPracticum/15th_sprint/Lib Dev & Large Projects/2 Virtual Methods Table/Vebinar/step2.cpp
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/2 Virtual Methods Table/Vebinar/step2.cpp
@@ -1,9 +1,11 @@
 class MagicHat {
 public:
     //используем using чтобы сделать код более читаемым
+    //здесь уходим от названия указателей
     using InsideFunction = void(*)(const MagicHat*);
     using DeleteFunction = void(*)(MagicHat*);
-    struct Vtable {       
+    struct Vtable {
+        //используем классические названия типов
         InsideFunction inside_this;
         DeleteFunction delete_this;
     };
