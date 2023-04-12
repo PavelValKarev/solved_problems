a7e469df8ef0522bc9b2b4cd904ab609503dc0ae
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-13 01:50:45 +0300

    create VTABLE

---
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/2 Virtual Methods Table/Vebinar/step4.cpp
@@ -0,0 +1,24 @@
+class MagicHat {
+public:
+    //используем using чтобы сделать код более читаемым
+    //здесь уходим от названия указателей
+    using InsideFunction = void(*)(const MagicHat*);
+    using DeleteFunction = void(*)(MagicHat*);
+
+    struct Vtable {
+        //используем классические названия типов
+        InsideFunction inside_this;
+        DeleteFunction delete_this;
+    };
+
+    //создаем указатель на таблицу и по умолчанию
+    //приравниваем его к nullptr
+    MagicHat::Vtable* vptr_ = nullptr;
+
+    //размещаем саму таблицу и называем ее VTABLE
+    MagicHat::Vtable VTABLE;
+    
+    virtual void WhatInside() const {
+        cout << "Nothing"s << endl;
+    }    
+};
