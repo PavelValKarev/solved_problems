c90ba743facbd2a8d7b0dc7ac50450a279077d62
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-14 16:29:23 +0300

    code for start

---
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/2 Virtual Methods Table/6 Pure Virtual/MagicHat.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//функция для печати адреса виртуальной таблицы
+void PrintVptr(void* obj) {
+    auto ptr = reinterpret_cast<size_t*>(obj);
+    cout << "Vptr : "s << *ptr << endl;
+}
+
+class MagicHat {
+public:
+    MagicHat() {
+        cout << "MagicHat::Ctor"s;
+        if (HasSomethingInside()) {
+            WhatInside();
+        }
+    }
+    ~MagicHat() {
+        cout << "MagicHat::Dtor"s;
+        PrintVptr(this);
+    }
+    virtual void IsMagicHat() const {
+        cout << "MagicHat:: I'm a magic hat"s << endl;
+    }
+    virtual void WhatInside() const {
+        cout << "Nothing"s << endl;
+    }
+    virtual bool HasSomethingInside() const {
+        cout << "Nope"s << endl;
+        return false;
+    }
+};
+
+class HatWithApples : public MagicHat {
+public:
+    void WhatInside() const {
+        cout << "Apple"s << endl;
+    }
+    bool HasComethingInside() const {
+        cout << "Yes. Something red and tasty"s << endl;
+        return true;
+    }
+
+private:
+    /* 10 яблок лежало в шляпе */
+    int apples_count = 10;
+};
+
+class HatWithRabbits : public MagicHat {
+public:
+    void WhatInside() const {
+        cout << "Rabbit"s << endl;
+    }
+    bool HasComethingInside() const {
+        cout << "Yes. Several furry carrot lovers"s << endl;
+        return true;
+    }
+
+private:
+    /* 3 кролика сидело в шляпе */
+    int rabbits_count = 3;
+    /* каждому кролику полагается две морковки, чтобы пережить фокус без стресса */
+    int carrot_count = 6;
+};
+
+class HatWithDoves : public MagicHat {
+public:
+    void WhatInside() const {
+        cout << "Dove"s << endl;
+    }
+    bool HasSomethingInside() const {
+        cout << "Yes. Flying simbol of peace"s << endl;
+        return true;
+    }
+};
+
+
+
+int main() {
+    HatWithApples apple_hat1;
+    HatWithApples apple_hat2;
+    HatWithRabbits rabbit_hat1;
+    HatWithRabbits rabbit_hat2;
+    HatWithDoves dove_hat1;
+    HatWithDoves dove_hat2;
+
+    vector<MagicHat*> hats = { &apple_hat1, &apple_hat2,
+                              &rabbit_hat1, &rabbit_hat2,
+                              &dove_hat1, &dove_hat2 };
+    for (auto hat : hats) {
+        PrintVptr(hat);
+    }
+
+    return 0;
+}
