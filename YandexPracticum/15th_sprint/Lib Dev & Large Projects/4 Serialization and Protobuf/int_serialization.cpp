f5a6f948048b85fb501c40257ad4c94d85bc43da
PavelValKarev <112899141+PavelValKarev@users.noreply.github.com>
2023-04-18 18:17:02 +0300

    Update int_serialization.cpp

--- YandexPracticum/15th_sprint/Lib Dev & Large Projects/4 Serialization and Protobuf/int_serialization.cpp
+++ YandexPracticum/15th_sprint/Lib Dev & Large Projects/4 Serialization and Protobuf/int_serialization.cpp
@@ -11,11 +11,16 @@ int main() {
     int32_t number_to_serialize = 31415927;
     int32_t read_number = 0;
     {
-        ofstream out_file("some_file.bin");
-        out_file.write(reinterpret_cast<const char*>(&number_to_serialize), sizeof(number_to_serialize));
+        /*
+        Если мы собираемся писать двоичные данные в файл, его нужно открыть с 
+        флагом std::ios::binary, иначе может записаться странная вещь.
+        */
+        ofstream out_file("some_file.bin", std::ios::binary);
+        out_file.write(
+            reinterpret_cast<const char*>(&number_to_serialize), sizeof(number_to_serialize));
     }
     {
-        ifstream in_file("some_file.bin");
+        ifstream in_file("some_file.bin", std::ios::binary);
         in_file.read(reinterpret_cast<char*>(&read_number), sizeof(read_number));
     }
     cout << "Прочитали "sv << read_number << ", ожидали "sv << number_to_serialize
